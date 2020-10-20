0519d1b3ec0fb34a813a1a5b874132db05c7115c
Eugene Polozov <eugene@retailcrm.es>
2020-10-20 14:00:35 +0300

    Update initial migration

--- src/Migrations/Version00000000000000.php
+++ src/Migrations/Version00000000000000.php
@@ -22,12 +22,12 @@ final class Version20201002074456 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE TABLE i_account (id UUID NOT NULL, integration_id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT \'now()\' NOT NULL, deactivated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, name VARCHAR(255) NOT NULL, account_id VARCHAR(255) NOT NULL, test BOOLEAN DEFAULT \'false\' NOT NULL, approve_manually BOOLEAN DEFAULT \'false\' NOT NULL, public_key VARCHAR(255) DEFAULT NULL, secret_key VARCHAR(255) DEFAULT NULL, locale VARCHAR(10) DEFAULT \'en_US\' NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE i_account (id UUID NOT NULL, integration_id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT now() NOT NULL, deactivated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, name VARCHAR(255) NOT NULL, account_id VARCHAR(255) NOT NULL, test BOOLEAN DEFAULT \'false\' NOT NULL, approve_manually BOOLEAN DEFAULT \'false\' NOT NULL, public_key VARCHAR(255) DEFAULT NULL, secret_key VARCHAR(255) DEFAULT NULL, locale VARCHAR(10) DEFAULT \'en_US\' NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX i_integration_id_idx ON i_account (integration_id)');
         $this->addSql('CREATE UNIQUE INDEX i_integration_id_account_id_idx ON i_account (integration_id, account_id)');
         $this->addSql('COMMENT ON COLUMN i_account.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN i_account.integration_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE i_integration (id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT \'now()\' NOT NULL, crm_url VARCHAR(255) NOT NULL, crm_api_key VARCHAR(255) NOT NULL, active BOOLEAN DEFAULT \'true\' NOT NULL, freezed BOOLEAN DEFAULT \'false\' NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE i_integration (id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT now() NOT NULL, crm_url VARCHAR(255) NOT NULL, crm_api_key VARCHAR(255) NOT NULL, active BOOLEAN DEFAULT \'true\' NOT NULL, freezed BOOLEAN DEFAULT \'false\' NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX i_integration_crm_url_api_key_idx ON i_integration (crm_url, crm_api_key)');
         $this->addSql('COMMENT ON COLUMN i_integration.id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE i_payment (id VARCHAR(255) NOT NULL, account_id UUID NOT NULL, url_id INT NOT NULL, status VARCHAR(255) DEFAULT NULL, paid BOOLEAN DEFAULT \'false\' NOT NULL, amount VARCHAR(255) DEFAULT NULL, refunded_amount VARCHAR(255) DEFAULT NULL, currency VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, captured_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, test BOOLEAN DEFAULT \'false\' NOT NULL, cancel_on_waiting_capture BOOLEAN DEFAULT \'false\' NOT NULL, invoice_uuid UUID NOT NULL, payment_uuid UUID NOT NULL, session_id VARCHAR(255) NOT NULL, cancellation_details VARCHAR(255) DEFAULT NULL, refundable BOOLEAN DEFAULT \'true\' NOT NULL, PRIMARY KEY(id))');
@@ -41,7 +41,7 @@ final class Version20201002074456 extends AbstractMigration
         $this->addSql('CREATE TABLE i_stripe_notification (id UUID NOT NULL, payment_id VARCHAR(255) DEFAULT NULL, response TEXT DEFAULT NULL, event VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_98178C6D4C3A3BB ON i_stripe_notification (payment_id)');
         $this->addSql('COMMENT ON COLUMN i_stripe_notification.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE i_stripe_webhook (id UUID NOT NULL, account_id UUID DEFAULT NULL, webhook VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT \'now()\' NOT NULL, secret VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE i_stripe_webhook (id UUID NOT NULL, account_id UUID DEFAULT NULL, webhook VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT now() NOT NULL, secret VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX i_account_id_idx ON i_stripe_webhook (account_id)');
         $this->addSql('COMMENT ON COLUMN i_stripe_webhook.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN i_stripe_webhook.account_id IS \'(DC2Type:uuid)\'');
